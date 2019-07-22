a4024604b63b77f4bb05bff61718792c1b2ee628
willyb321 <wbwilliam7@gmail.com>
2019-07-23 06:20:30 +1000

    fix timestamp,embed,add,removeguild

--- audit-log.js
+++ audit-log.js
@@ -2,7 +2,8 @@ const commando = require('discord.js-commando');
 const Discord = require('discord.js');
 
 const genEmbed = () => new Discord.MessageEmbed()
-    .setFooter('Galactic Academy Bot - By Willyb321');
+    .setFooter('Galactic Academy Bot - By Willyb321')
+    .setTimestamp();
 
 module.exports = async function logEvents(client) {
     client.on("messageDelete", async (messageDelete) => {
@@ -20,6 +21,9 @@ module.exports = async function logEvents(client) {
         log.send({embed});
     });
     client.on("messageUpdate", async (oldMessage, newMessage) => {
+        if (oldMessage.content === newMessage.content) {
+            return;
+        }
         const logChannel = await oldMessage.guild.settings.get('logChannel');
         const log = client.channels.get(logChannel);
         if (!log) {
@@ -34,4 +38,35 @@ module.exports = async function logEvents(client) {
             .addField('New message', newMessage.content)
         log.send({embed});
     });
+
+    client.on('guildMemberAdd', member => {
+        const guild = member.guild;
+        const logChannelID = guild.settings.get('logChannel');
+        const channel = guild.channels.get(logChannelID);
+        if (!channel) {
+            return;
+        }
+        const embed = genEmbed();
+        embed
+            .setTitle(`Someone joined ${member.guild.name}`)
+            .setDescription(`Name: ${member.toString()}`)
+            .addField('ID', member.user.id);
+            channel.send({embed});
+    });
+
+    client.on('guildMemberRemove', member => {
+        const guild = member.guild;
+        const logChannelID = guild.settings.get('logChannel');
+        const channel = guild.channels.get(logChannelID);
+        if (!channel) {
+            return;
+        }
+        const embed = genEmbed();
+        embed
+            .setTitle(`Someone left ${member.guild.name}`)
+            .setDescription(`Name: ${member.user.tag}`)
+            .addField('ID', member.user.id);
+        channel.send({embed});
+    });
+
 }
